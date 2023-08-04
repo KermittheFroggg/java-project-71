b6e64dda2713c652d74c04b8ac74a1165af773da
KermittheFroggg <santamantana@gmail.com>
2023-08-04 17:58:12 +0300

    step 9

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -35,7 +35,10 @@ public class Formatter {
                                Map<String, Object> resultOfDiffer) {
         String resultString = "";
         for (Map.Entry<String, Object> entry : resultOfDiffer.entrySet()) {
-            addQuotesToString(map1, map2, entry);
+
+            addQuotesToString(map1, entry);
+            addQuotesToString(map2, entry);
+
             switch (entry.getValue().toString()) {
                 case "updated":
                     checkComplexValue(map1, entry);
@@ -58,14 +61,10 @@ public class Formatter {
         return resultString.trim();
     }
 
-    public static void addQuotesToString(Map<String, Object> map1,
-                                         Map<String, Object> map2,
+    public static void addQuotesToString(Map<String, Object> map,
                                          Map.Entry<String, Object> entry) {
-        if (map1.get(entry.getKey()) instanceof String) {
-            map1.put(entry.getKey(), "'" + map1.get(entry.getKey()) + "'");
-        }
-        if (map2.get(entry.getKey()) instanceof String) {
-            map2.put(entry.getKey(), "'" + map2.get(entry.getKey()) + "'");
+        if (map.get(entry.getKey()) instanceof String) {
+            map.put(entry.getKey(), "'" + map.get(entry.getKey()) + "'");
         }
     }
 
