77c8c4edfdd7c6fb4463ff2bc9ea5cbe911caf43
KermittheFroggg <santamantana@gmail.com>
2023-08-04 16:43:25 +0300

    step 9

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -42,30 +42,6 @@ public class Differ {
     }
 
     public static String generate(String file1, String file2) throws Exception {
-
-        Map<String, Object> map1 = Parser.parser(file1);
-        Map<String, Object> map2 = Parser.parser(file2);
-
-        Set<String> keys = new TreeSet<>();
-        keys.addAll(map1.keySet());
-        keys.addAll(map2.keySet());
-
-        Map<String, Object> result = new LinkedHashMap<>();
-        for (String key : keys) {
-            if (map1.containsKey(key)) {
-                if (map2.containsKey(key)) {
-                    if (Objects.equals(map1.get(key), map2.get(key))) {
-                        result.put(key, "same");
-                    } else {
-                        result.put(key, "updated");
-                    }
-                } else {
-                    result.put(key, "removed");
-                }
-            } else {
-                result.put(key, "added");
-            }
-        }
-        return Formatter.stylish(map1, map2, result);
+        return generate(file1, file2, "stylish");
     }
 }
