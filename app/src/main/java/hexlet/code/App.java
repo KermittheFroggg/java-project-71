c877de4ca8eafbcbe76fd5fd16257872e65612c7
KermittheFroggg <santamantana@gmail.com>
2023-08-07 14:55:55 +0300

    step 10

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,17 +7,17 @@ import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable {
+public final class App implements Callable {
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
     }
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", defaultValue = "stylish")
-    String format;
+    private String format;
     @Parameters(paramLabel = "filepath1", description = "path to first file")
-    String file;
+    private String file;
     @Parameters(paramLabel = "filepath2", description = "path to second file")
-    String file2;
+    private String file2;
     @Override
     public Object call() {
         try {
